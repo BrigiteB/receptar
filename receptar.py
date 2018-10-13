680aa4e15109bd41fa52f4feb16f94ca4ec693f5
Bara Becvarova <barborabecvarova86@gmail.com>
2018-10-13 15:55:17 +0200

    Privitani uzivatele

--- receptar.py
+++ receptar.py
@@ -12,7 +12,7 @@ def nacti_recept(jmeno):
     adresar_receptu = os.path.join(adresar_projektu, 'recepty')
     cesta_k_receptu = os.path.join(adresar_receptu, jmeno)
     try:
-        with open(cesta_k_receptu, 'r') as f:
+        with open(cesta_k_receptu, 'r', encoding="utf8") as f:
             return f.read()
     except FileNotFoundError as e:
         sys.exit('Bohuzel soubor s receptem neexistuje: {}'.format(cesta_k_receptu))
@@ -21,6 +21,13 @@ def nacti_recept(jmeno):
 NAZVY_RECEPTU = [
     'chlebova_pochoutka.txt',
 ]
+
+print("______ _____ _____  ___________ _____ ___  ______")
+print("| ___ \  ___/  __ \|  ___| ___ \_   _/ _ \ | ___ \ ")
+print("| |_/ / |__ | /  \/| |__ | |_/ / | |/ /_\ \| |_/ /")
+print("|    /|  __|| |    |  __||  __/  | ||  _  ||    /")
+print("| |\ \| |___| \__/\| |___| |     | || | | || |\ \ ")
+print("\_| \_\____/ \____/\____/\_|     \_/\_| |_/\_| \_|")
 
 
 # Máme zatím jen jeden recept, tak ho prostě ukážeme
